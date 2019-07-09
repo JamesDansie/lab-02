7cabf28b244db95262db53a26a2ab0df60ea42ac
James Dansie <jdansie8336@gmail.com>
2019-07-09 13:33:06 -0700

    initial commit of site

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// var likesStrangerThings = prompt('Do you like Stranger Things?');
+
+// if(likesStrangerThings.toLowerCase() === 'yes'){
+//     alert('I like it too! :D')
+// } else {
+//     alert('You fool!')
+// }
+
+var likesChineseFood = prompt('Do you like Chinese food?');
+
+if(likesChineseFood.toLowerCase() === 'yes'){
+    alert('Go to Spice Up in Belltown and get Mopa Tofu and Pork Belly')
+} else if(likesChineseFood.toLowerCase() === 'maybe'){
+    alert('Pho is pretty good too.')
+}else {
+    alert('Why?! Who doesn\' like Chinese food?!');
+}
