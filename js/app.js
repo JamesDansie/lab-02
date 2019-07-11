53fdef7951a343e6537b995f434dffbb27024349
Julie Erlemeier <jmerlemeier@gmail.com>
2019-07-11 16:32:52 -0700

    Started functions for Q6. Had fun.

--- js/app.js
+++ js/app.js
@@ -3,76 +3,84 @@
 var userName = prompt('What is your name?');
 var correctGuess = 0;
 
-//quest prompt
-var quest = prompt('Is your quest to seek the holy grail?');
-if(quest.toLowerCase() === 'y' || quest.toLowerCase() === 'yes'){
-    //color prompt
-    correctGuess++;
-    var color = prompt('Is your favorite color green?');
-    if(color.toLowerCase() === 'y' || color.toLowerCase() === 'yes'){
-        //swallow prompt
-        correctGuess++;
-        var swallowSpeed = prompt('Do you know the speed of laden swallow?');
-        if(swallowSpeed.toLowerCase() === 'n' || swallowSpeed.toLowerCase() ==='no'){
-            //bring out your dead
-            correctGuess++;
-            var dead = prompt('Did you bring out your dead?');
-            if(dead.toLowerCase() === 'y' || dead.toLowerCase() === 'yes'){
-                //flesh wound
-                correctGuess++;
-                var fleshWound = prompt('Is that mearly a flesh wound?');
-                if(fleshWound.toLowerCase() === 'y' || fleshWound.toLowerCase() === 'yes'){
-                    correctGuess++;
-                    alert(userName + "! You are a true monty python nerd. Good job friend. Now go outside, it's good for you.");
-                }else{
-                    alert('Go watch Monty Python Holy Grail');
-                }
-            }else{
-                alert('Go watch Monty Python Holy Grail ' + userName);
+// //quest prompt
+// var quest = prompt('Is your quest to seek the holy grail?');
+// if(quest.toLowerCase() === 'y' || quest.toLowerCase() === 'yes'){
+//     //color prompt
+//     correctGuess++;
+//     var color = prompt('Is your favorite color green?');
+//     if(color.toLowerCase() === 'y' || color.toLowerCase() === 'yes'){
+//         //swallow prompt
+//         correctGuess++;
+//         var swallowSpeed = prompt('Do you know the speed of laden swallow?');
+//         if(swallowSpeed.toLowerCase() === 'n' || swallowSpeed.toLowerCase() ==='no'){
+//             //bring out your dead
+//             correctGuess++;
+//             var dead = prompt('Did you bring out your dead?');
+//             if(dead.toLowerCase() === 'y' || dead.toLowerCase() === 'yes'){
+//                 //flesh wound
+//                 correctGuess++;
+//                 var fleshWound = prompt('Is that mearly a flesh wound?');
+//                 if(fleshWound.toLowerCase() === 'y' || fleshWound.toLowerCase() === 'yes'){
+//                     correctGuess++;
+//                     alert(userName + "! You are a true monty python nerd. Good job friend. Now go outside, it's good for you.");
+//                 }else{
+//                     alert('Go watch Monty Python Holy Grail');
+//                 }
+//             }else{
+//                 alert('Go watch Monty Python Holy Grail ' + userName);
+//             }
+//         }else{
+//             alert('Go watch Monty Python Holy Grail ' + userName);
+//         }
+//     } else {
+//         alert('Go watch Monty Python Holy Grail ' + userName);
+//     }
+// } else {
+//     alert('Go watch Monty Python Holy Grail ' + userName);
+// }
+
+// console.log('the userName is ' + userName);
+// console.log('the quest var is ' + quest);
+// console.log('the color var is ' + color);
+// console.log('the swallowSpeed var is ' + swallowSpeed);
+// console.log('the dead var is ' + dead);
+// console.log('the fleshWound var is ' + fleshWound);
+
+// -------------------6th question------------------//
+
+function question6(maxGuess=4, answer=42){
+    var numGuess = 1;
+    while(numGuess <= maxGuess){
+        
+        //input validation
+        while(true){
+            var userNum = prompt('From Hitch Hiker\'s Guide to the Galaxy, what is the meaning of life?');
+            userNum = Number(userNum);
+            console.log('The userNum is '+userNum+', and the nmaxGuess is '+maxGuess+', and the answer is '+answer);
+            
+            //If it is not a number it will return NaN which will be false in an if statement
+            if(userNum){
+                break;
+            } else {
+                alert('Pick a number');
             }
-        }else{
-            alert('Go watch Monty Python Holy Grail ' + userName);
         }
-    } else {
-        alert('Go watch Monty Python Holy Grail ' + userName);
-    }
-} else {
-    alert('Go watch Monty Python Holy Grail ' + userName);
-}
 
-console.log('the userName is ' + userName);
-console.log('the quest var is ' + quest);
-console.log('the color var is ' + color);
-console.log('the swallowSpeed var is ' + swallowSpeed);
-console.log('the dead var is ' + dead);
-console.log('the fleshWound var is ' + fleshWound);
-
-//6th question
-var numGuess = 1;
-while(numGuess <= 4){
-    //input validation
-    while(true){
-        var userNum = prompt('From Hitch Hiker\'s Guide to the Galaxy, what is the meaning of life?');
-        userNum = Number(userNum);
-        console.log('The user number is for hitch hike is '+userNum);
-        //If it is not a number it will return NaN which will be false in an if statement
-        if(userNum){
+        if(userNum === answer){
+            alert('Good job ' + userName +'! Nerd skills +1000');
+            correctGuess++;
             break;
-        } else {
-            alert('Pick a number');
+        }else if(userNum < answer){
+            alert(userName + ' a little higher (also go read Hitch Hiker\'s guide (and bring a towel). You have '+(maxGuess-numGuess)+' guesses left.');
+        }else{
+            alert(userName + ' guess lower, and read Hitch Hiker\'s guide (and bring a towel). You have '+(maxGuess-numGuess)+' guesses left.');
         }
+        numGuess++
     }
-    if(userNum === 42){
-        alert('Good job ' + userName +'! Nerd skills +1000');
-        correctGuess++;
-        break;
-    }else if(userNum < 42){
-        alert(userName + ' a little higher (also go read Hitch Hiker\'s guide (and bring a towel). You have '+(4-numGuess)+' guesses left.');
-    }else{
-        alert(userName + ' guess lower, and read Hitch Hiker\'s guide (and bring a towel). You have '+(4-numGuess)+' guesses left.');
-    }
-    numGuess++
+
 }
+question6(2, 1000);
 
 //7th question
 var characters = ['Marvin the Paranoid Andriod', 'Ford Perfect', 'Dirk Gently', 'Authur Dent', 'Trillian', 'Random Dent', 'Zaphod Beeblebrox', 'Slartibartfast', 'George Gently']
